23d8f2e8f5007fb79603556b50c79a6fc9e2275a
Pretsnor <mishapaauw@gmail.com>
2016-11-25 13:23:51 +0100

    geen assen

--- Homework/week-4/barchart.js
+++ Homework/week-4/barchart.js
@@ -15,15 +15,29 @@ data is crab measurements
 reference paper crabs: http://duch.mimuw.edu.pl/~pokar/StatystykaII/DANE/CampbellMahon.pdf
 *******/
 
-var svg = d3.select("svg"),
-    margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom;
+var margin = {top: 20, right: 30, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
-    y = d3.scaleLinear().rangeRound([height, 0]);
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
 
-var g = svg.append("g")
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10);
+
+var chart = d3.select(".chart")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // Load data and construct graph
@@ -36,35 +50,45 @@ d3.json("crabs.json", function(data) {
 		data[i].CW = +data[i].CW;
 		data[i].RW = +data[i].RW;
 	}
+
+	console.log(data)
 	
-	// get data domains
+	// get data domains, max height gets some extra margin
+	var extraMargin = 5;
 	x.domain(data.map(function(d) { return d.speciessex; }))  
-	y.domain([0, d3.max(data, function(d) { return d.CL; })]);
+	y.domain([0, d3.max(data, function(d) { return d.CL; }) + extraMargin]);
 
-	// x axis
-	g.append("g")
-      .attr("class", "axis axis--x")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+	chart.append("g")
+      	.attr("class", "x axis")
+      	.attr("transform", "translate(0," + height + ")")
+      	.call(xAxis);
 
-  	// y axis
-  	g.append("g")
-      .attr("class", "axis axis--y")
-      .call(d3.axisLeft(y).ticks(5))
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end")
-      .text("Frequency");
+  	chart.append("g")
+      	.attr("class", "y axis")
+      	.call(yAxis);
 
-  	g.selectAll(".bar")
-    .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.speciessex); })
-      .attr("y", function(d) { return y(d.CL); })
-      .attr("width", x.bandwidth())
-      .attr("height", function(d) { return height - y(d.CL); });
+  	chart.selectAll(".bar")
+    	.data(data)
+      .enter().append("rect")
+      	.attr("class", "bar")
+      	.attr("x", function(d) { return x(d.speciessex); })
+     	.attr("y", function(d) { return y(d.CL); })
+      	.attr("height", function(d) { return height - y(d.CL); })
+      	.attr("width", x.rangeBand())
+      	// mouse over color change
+     	.on("mouseover", function(d) {
+      		d3.select(this).transition()
+      			.ease("elastic")
+      			.duration(3000)
+      			.style("fill", "#96C0CE");
+      	})
+      	// mouseout color change
+      	.on("mouseout", function(d){
+      		d3.select(this).transition()
+      			.ease("elastic")
+      			.duration(2500)
+      			.style("fill", "orange")
+      	});
+
 });
 	
